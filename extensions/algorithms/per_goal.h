c397637728e7266951e6f54f0da93ec72e5ea5b6
Meir Goldenberg <mgoldenbe@gmail.com>
2016-05-30 09:42:38 +0300

    Documented astar.h and minor changes.

--- extensions/algorithms/per_goal.h
+++ extensions/algorithms/per_goal.h
@@ -1,16 +1,27 @@
 #ifndef PER_GOAL_H
 #define PER_GOAL_H
 
+/// \file
+/// \brief The \ref PerGoal class.
+/// \author Meir Goldenberg
+
 #include "algorithm.h"
 
-// Per-Goal is a high-level algorithm that runs some other algorithm for each
-// goal and combined results
+/// \c PerGoal is a meta search algorithm for solving instances with multiple
+/// goals. It runs some other search algorithm for each goal state and combines
+/// results. Each goal state is solved independently of all others.
+/// \tparam MyAlgorithm The search algorithm to run for each goal state.
 template <ALG_TPARAMS, class MyAlgorithm = SLB_RAW_ALGORITHM<logFlag> >
 struct PerGoal : Algorithm<ALG_TARGS> {
     ALG_TYPES
     ALG_DATA
-    using Base::Algorithm;
+    using Base::Algorithm; ///< Using the base's constructors.
 
+    /// Runs the algorithm.
+    /// \return The solution cost, which is the average of solution costs among
+    /// the goals.
+    /// \warning There is no special treatment for instances that do not have a
+    /// solution at this point.
     CostType run() {
         CostType res = 0.0;
         for (auto i : instance_.goalInstances()) {
@@ -21,11 +32,17 @@ struct PerGoal : Algorithm<ALG_TARGS> {
         return static_cast<double>(res) / instance_.goalInstances().size();
     }
 
+    /// Returns the statistics about the underlying search algorithm's
+    /// performance for solving the particular instance for all goal states.
+    /// \return The statistics about the underlying search algorithm's
+    /// performance for solving the particular instance for all goal states.
     MeasureSet measures() const {
         return stats_.sum(true)[0];
     }
 
 private:
+    /// Statistics about the underlying search algorithm's performance for
+    /// solving for all goal states.
     Stats stats_;
 };
 
