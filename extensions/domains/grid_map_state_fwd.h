cd548b11e49a31ef60e9b83314ca7c74d6030131
Meir Goldenberg <mgoldenbe@gmail.com>
2016-09-22 11:43:59 +0300

    Forgotten files.

---
+++ extensions/domains/grid_map_state_fwd.h
@@ -0,0 +1,13 @@
+#ifndef GRID_MAP_STATE_FWD_H
+#define GRID_MAP_STATE_FWD_H
+
+/// \file
+/// \brief Forward declarations for \ref grid_map_state.h.
+/// \author Meir Goldenberg
+
+namespace Domains {
+template <typename = SLB_COST_TYPE, bool = SLB_UNIFORM_DOMAIN>
+struct GridMapState;
+}
+
+#endif
---
+++ extensions/domains/headers_fwd.h
@@ -0,0 +1,12 @@
+#ifndef SLB_DOMAINS_HEADERS_FWD_H
+#define SLB_DOMAINS_HEADERS_FWD_H
+
+/// \file
+/// \brief Forward declaration headers.
+/// \author Meir Goldenberg
+
+#include "extensions/domains/pancake_fwd.h"
+#include "extensions/domains/grid_map_state_fwd.h"
+#include "extensions/domains/inc_worst_fwd.h"
+
+#endif
