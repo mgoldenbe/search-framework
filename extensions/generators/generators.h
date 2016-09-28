58dc0056950e90cc7803d820283e50feb152bd9f
Meir Goldenberg <mgoldenbe@gmail.com>
2016-09-28 11:44:48 +0300

    Oops. Also forgotten!

---
+++ extensions/generators/generators.h
@@ -0,0 +1,92 @@
+#ifndef GENERATORS_H
+#define GENERATORS_H
+
+/// \file
+/// \brief Generators to be used by the search algorithms.
+/// \author Meir Goldenberg
+
+/// Generator that generates state neighbors.
+/// \tparam MyAlgorithm The search algorithm.
+template <class MyAlgorithm, template <class> class Heuristic>
+struct StatesGenerator {
+    POLICY_TYPES
+
+    using Neighbor = typename State::SNeighbor;
+
+    /// The contructor.
+    /// \param alg Reference to the search algorithm.
+    StatesGenerator(MyAlgorithm &alg) : alg_(alg), heuristic_(alg) {}
+
+    /// Returns the neighbor state.
+    /// \return Reference to the neighbor state.
+    /// \note This reference must be non-const to allow moving the neighbor. In
+    /// addition, this member could be static; made it non-static for reasons of
+    /// uniformity.
+    State &state(Neighbor &n) { return n.state(); }
+
+    /// Computes the heuristic.
+    /// \param n The neighbor for which the heuristic is to be computed.
+    /// \param node The search node for which the heuristic is to be computed.
+    /// \return The heuristic value.
+    CostType heuristic(const Neighbor &n, Node *node) const {
+        return heuristic_(n, node);
+    }
+
+    /// Computes the successors of the given state.
+    /// \param s The state.
+    /// \return The vector of successors.
+    /// \note This member could be static; made it non-static for reasons of
+    /// uniformity.
+    std::vector<Neighbor> successors(const State &s) const {
+        return s.stateSuccessors();
+    }
+private:
+    MyAlgorithm &alg_; ///< Reference to the search algorithm.
+    Heuristic<MyAlgorithm> heuristic_; ///< The heuristic.
+};
+template <class MyAlgorithm>
+using StatesGeneratorPolicy = StatesGenerator<MyAlgorithm, SLB_HEURISTIC>;
+
+/// Generator that generates action neighbors.
+/// \tparam MyAlgorithm The search algorithm.
+template <class MyAlgorithm, template <class> class Heuristic>
+struct ActionsGenerator {
+    POLICY_TYPES
+
+    using Neighbor = typename State::ANeighbor;
+
+    /// The contructor.
+    /// \param alg Reference to the search algorithm.
+    ActionsGenerator(MyAlgorithm &alg) : alg_(alg), heuristic_(alg) {}
+
+    /// Returns the neighbor state.
+    /// \return Reference to the neighbor state.
+    /// \note This reference must be non-const to allow moving the neighbor
+    State state(Neighbor &n) const {
+        State res{alg_.cur()->state()};
+        return res.apply(n.action());
+    }
+
+    /// Computes the heuristic.
+    /// \param n The neighbor for which the heuristic is to be computed.
+    /// \return The heuristic value.
+    CostType heuristic(const Neighbor &n, Node *node) const {
+        return heuristic_(n, node);
+    }
+
+    /// Computes the successors of the given state.
+    /// \param s The state.
+    /// \return The vector of successors.
+    /// \note This member could be static; made it non-static for reasons of
+    /// uniformity.
+    std::vector<Neighbor> successors(const State &s) const {
+        return s.actionSuccessors();
+    }
+private:
+    MyAlgorithm &alg_; ///< Reference to the search algorithm.
+    Heuristic<MyAlgorithm> heuristic_; ///< The heuristic.
+};
+template <class MyAlgorithm>
+using ActionsGeneratorPolicy = ActionsGenerator<MyAlgorithm, SLB_HEURISTIC>;
+
+#endif
