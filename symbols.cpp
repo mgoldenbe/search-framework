0bcb6c102b5ccf69b288c8599c30bee700787168
Meir Goldenberg <mgoldenbe@gmail.com>
2016-11-16 18:19:43 +0200

    symbols.cpp works for undefined symbols used as default template arguments.

--- symbols.cpp
+++ symbols.cpp
@@ -2,6 +2,22 @@
 #include <fstream>
 #include <regex>
 
+/// \file
+/// \brief The tool for getting rid of undefined symbols used as default
+/// template arguments.
+///
+/// The symbol may stand for a template, so that we might need to replace a
+/// symbol with its own template arguments. The tool throws an exception if the
+/// brackets in the latter case are not matched (nested templates are handled).
+/// It also throws an exception if an indication is found that an undefined
+/// symbol is used in the context other than a default template argument.
+///
+/// \author Meir Goldenberg
+
+/// Finds the matching closing bracket.
+/// \param str The source string.
+/// \param it Iterator to the position right after the opening bracket.
+/// \return Iterator to the matching closing bracket.
 std::string::const_iterator matchBracket(const std::string &str,
                                          std::string::const_iterator it) {
     if (it == str.cbegin()) throw std::runtime_error("Matching error.");
@@ -16,30 +32,98 @@ std::string::const_iterator matchBracket(const std::string &str,
     return it;
 }
 
+/// Finds the matching closing bracket.
+/// \param str The source string.
+/// \param pos Position right after the opening bracket.
+/// \return Position of the matching closing bracket.
+int matchBracket(const std::string &str, int pos) {
+    return matchBracket(str, str.cbegin() + pos) - str.cbegin();
+}
+
+// http://stackoverflow.com/a/37259671/2725810
+/// Computes the matches of a regex search.
+/// \param s The string in which to search.
+/// \param exp The regular expression.
+/// \return The matches of the regex search.
+std::vector<std::smatch> matches(std::string s, std::string exp) {
+    std::vector<std::smatch> res;
+    std::regex r{exp};
+    for (auto i = std::sregex_iterator(s.begin(), s.end(), r);
+         i != std::sregex_iterator(); ++i)
+        res.push_back(*i);
+    return res;
+}
+
+/// Computes the sub-string of the given string.
+/// \param s The string.
+/// \param b The beginning position of the range.
+/// \param s The position one beyond the end of the range.
+std::string mysubstr(std::string s, int b, int e) { return s.substr(b, e - b); }
+
+/// Forms a message to the user.
+std::string message(std::string s) {
+    return "Symbols Tool: " + s;
+}
+
+/// Prints an error message.
+void error(std::string s) {
+    std::cerr << message("Error: " + s) << std::endl;
+}
+
+/// Prints a fatal error message and exits.
+void fatal(std::string s) {
+    std::cerr << message("Fatal Error: " + s + " Exiting...") << std::endl;
+    exit(-1);
+}
+
+/// The main function.
 int main(int argc, char **argv) {
     if (argc < 2)
         throw std::invalid_argument("Need to supply the input file");
-    std::string target;
-    std::ifstream in(argv[1]);
-    if (!in)
-        throw std::invalid_argument("Could not open file " +
-                                    std::string(argv[1]));
-    std::string str((std::istreambuf_iterator<char>(in)),
-                    std::istreambuf_iterator<char>());
-    std::regex exp(R"(SLB_\w*(\s*<)?)");
-    std::smatch res;
 
-    // http://stackoverflow.com/a/35026140/2725810
-    // A simpler way: http://stackoverflow.com/a/37259671/2725810
-    std::string::const_iterator curStart( str.cbegin() );
-    while (regex_search(curStart, str.cend(), res, exp)) {
-      auto replaceBegin = curStart + res.position();
-        target += std::string(curStart, replaceBegin) + "Nothing";
-        curStart = matchBracket(str, replaceBegin + res.length());
-	std::cout << std::string(replaceBegin, curStart) << " --> Nothing" << std::endl;
+    std::ifstream in(argv[1]);
+    if (!in) fatal("Could not open file " + std::string(argv[1]));
+    // The initial source code
+    std::string source((std::istreambuf_iterator<char>(in)),
+                       std::istreambuf_iterator<char>());
+
+    // The target source code
+    std::string target = "";
+
+    int segmentBegin = 0;
+    for (const auto &m: matches(source, R"(=\s*SLB_\w*\s*<?)")) {
+        // Keep the part of the string up to the beginning of the pattern.
+        target += mysubstr(source, segmentBegin, m.position());
+
+        // After pattern or closing bracket if there was one.
+        segmentBegin = matchBracket(source, m.position() + m.length());
+
+        std::string forRemoval = mysubstr(source, m.position(), segmentBegin);
+
+        // Check that we are in the right context: the next non-whitespace
+        // character is either a comma or a closing bracket.
+        auto nonspace = find_if(source.cbegin() + segmentBegin, source.cend(),
+                                std::not1(std::ref(isspace)));
+        if (*nonspace != ',' && *nonspace != '>')
+            fatal("Undefined symbol " + forRemoval +
+                    " not in the default template argument context.");
+
+        std::cout << message("Removing: ") << forRemoval << std::endl;
     }
-    target += std::string(curStart, str.cend());
-    std::ofstream out(argv[1]);
+    target += mysubstr(source, segmentBegin, source.size() - 1);
+
+    // Checking that the target string does not contain any undefined symbols.
+    bool errorFlag = false;
+
+    for (const auto &m: matches(target, R"(SLB_\w*)")) {
+        errorFlag = true;
+        error("Undefined symbol " +
+              m.str() + " not in the default template argument context.");
+    }
+    if (errorFlag)
+        fatal("There are undefined symbols not in the default template "
+                "argument context.");
+    std::ofstream out(std::string{argv[1]} + ".changed");
     out << target << std::endl;
     return 0;
 }
--- temp/temp.cpp
+++ temp/temp.cpp
@@ -8,23 +8,30 @@
 #include <iomanip>
 #include <sstream>
 
-struct A{};
+#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
 
-void f(const A &a) {
-    (void)a;
-    std::cout << "f &" << std::endl;
-}
-
-void f(A &&a) {
-    (void)a;
-    std::cout << "f &&" << std::endl;
-}
-
-void g(A &&a) {
-    f(std::forward<A>(a));
+std::vector<std::smatch> matches(std::string s, std::string exp) {
+    std::vector<std::smatch> res;
+    std::regex reg{exp};
+    for (auto i = std::sregex_iterator(s.begin(), s.end(), std::regex{exp});
+         i != std::sregex_iterator(); ++i) 
+      res.push_back(*i);
+    return res;
 }
 
 int main() {
-    g(A{});
+    std::regex r(R"(\w)");
+    std::string s("a,b,c,d,e,f,g");
+    for(std::sregex_iterator i = std::sregex_iterator(s.begin(), s.end(), r);
+                             i != std::sregex_iterator();
+                             ++i)
+    {
+        std::smatch m = *i;
+        std::cout << "Match value: " << m.str() << " at Position " << m.position() << '\n';
+    }
     return 0;
 }
+
