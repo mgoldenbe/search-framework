1f4431ebb04b23ac6e4c74ae188d2c3c2dc766e8
Meir Goldenberg <mgoldenbe@gmail.com>
2017-03-14 14:51:47 +0200

    Added the explanation of the currently used policies.

---
+++ documentation/html/search/enums_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['vertexfillpattern',['VertexFillPattern',['../namespaceslb_1_1core_1_1ui.html#a0d123eba2567be5964595af181c9c3d1',1,'slb::core::ui']]],
+  ['vertexshape',['VertexShape',['../namespaceslb_1_1core_1_1ui.html#a5bba432158f178915f460bdcf8a047a7',1,'slb::core::ui']]],
+  ['visualizer_5fstate',['VISUALIZER_STATE',['../structslb_1_1core_1_1ui_1_1VisualizerData.html#a38e4b90b6d4faae3c15629f715440ad9',1,'slb::core::ui::VisualizerData']]]
+];
