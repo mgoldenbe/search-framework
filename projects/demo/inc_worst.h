81af82ceb11ccee82c18a70fa31dfd380e4dd931
mgoldenbe <mgoldenbe@gmail.com>
2018-07-04 15:06:52 +0300

    Fixed demo configuration files.

--- projects/demo/inc_worst.h
+++ projects/demo/inc_worst.h
@@ -6,7 +6,10 @@
 #define SLB_STATE ext::domain::incWorst::IncWorst
 #define SLB_COST_TYPE int
 #define SLB_UNIFORM_DOMAIN false
-#define SLB_CMD_LINE_ADD ext::domain::incWorst::CommandLine
+
+#define SLB_CMD_T_PARAM	class Add = ext::domain::incWorst::CommandLine
+#define SLB_CMD_T_ARG Add
+#define SLB_CMD_INIT Add(cmd_)
 
 #undef SLB_HEURISTIC
 #define SLB_HEURISTIC ext::policy::heuristic::Zero
--- projects/demo/pancake.h
+++ projects/demo/pancake.h
@@ -4,7 +4,11 @@
 #define SLB_COST_TYPE int
 #define SLB_UNIFORM_DOMAIN true
 #define SLB_STATE ext::domain::pancake::Pancake
-#define SLB_CMD_LINE_ADD ext::domain::pancake::CommandLine
+
+#define SLB_CMD_T_PARAM	class Add = ext::domain::pancake::CommandLine
+#define SLB_CMD_T_ARG Add
+#define SLB_CMD_INIT Add(cmd_)
+
 #define SLB_BASE_HEURISTIC ext::domain::pancake::GapHeuristic
 #define SLB_BASE_DYNAMIC_HEURISTIC ext::domain::pancake::DynamicGapHeuristic
 
